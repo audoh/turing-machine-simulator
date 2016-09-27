10b82db8987447233a4233823178ad152ad22df8
kookas <kookas.mail@gmail.com>
2016-09-27 03:23:28 +0100

    Added Tape and HalfTape classes ready for full tape implementation.

--- turing.py
+++ turing.py
@@ -14,6 +14,72 @@ class TapeRunoff(Exception):
 	def __str__(self):
 		return repr("Attempted to run off the left-side of the tape.")
 
+class HalfTape:
+	'''
+	Half of a tape for a Turing machine.
+	'''
+
+	def __init__(self, array = []):
+		self._tape = list(array)
+
+	def __str__(self):
+		return str(self._tape)
+
+	def __add__(self, operand):
+		return HalfTape(self._tape + operand._tape)
+
+	def _extend(self, index):
+		dif = index + 1 - len(self)
+
+		if dif > 0:
+			self._tape.extend(['_'] * dif)
+			return True
+		else:
+			return False
+
+	def __getitem__(self, index):
+		self._extend(index)
+		return self._tape[index]
+
+	def __setitem__(self, index, value):
+		self._extend(index)
+		self._tape[index] = value
+
+	def __len__(self):
+		return len(self._tape)
+
+class Tape:
+	'''
+	Tape for a Turing machine.
+	'''
+
+	def __init__(self, array):
+		self._right = HalfTape(array)
+		self._left = HalfTape()
+
+	def __str__(self):
+		return str(HalfTape(reversed(self._left)) + self._right)
+
+	def __getitem__(self, index):
+		if index < 0:
+			index = -index - 1
+			return self._left[index]
+		else:
+			return self._right[index]
+
+	def __setitem__(self, index, value):
+		if index < 0:
+			index = -index - 1
+			self._left[index] = value
+		else:
+			self._right[index] = value
+
+t = Tape([1,2,3,4])
+
+t[-2] = 1
+
+print(t)
+
 class TuringMachine:
 	'''
 	A half-tape Turing machine.
@@ -59,7 +125,8 @@ class TuringMachine:
 	state = 1
 	head = 0
 	rule = None
-	_tape = ''
+
+	_tape = []
 
 	@property
 	def tape(self):
